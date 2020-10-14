dfe55fba0b4c13b23f7cad1ab4b7645ee9811e44
Vlados <vlad8aromanov@gmail.com>
2020-10-14 18:14:13 +0300

    The first commit

---
+++ src/com/company/God.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class God {
+
+    private static God god;
+
+    private God(){
+        System.out.println("I am the only God here!");
+    }
+
+    public static God getInstance(){
+        if (god == null){
+            return new God();
+        }
+        return god;
+    }
+
+
+    public void createUniverse(){
+        System.out.println("Universe has been created!");
+    }
+
+    public void createTheFirstHuman(){
+        System.out.println("The first human has been created!");
+    }
+
+    public void startTheTimeLine(){
+        System.out.println("Timeline has been started");
+    }
+
+}
