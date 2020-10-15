972cd76233f349158159e70aaecee983d9612bfa
Chepush <47068825+Chepush@users.noreply.github.com>
2020-10-15 16:28:12 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int a = 1;
-        System.out.println(a);
         God god = God.getInstance();
 
     }
